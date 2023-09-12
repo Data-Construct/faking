156f13ee8d34b5334555f9f645645e26a058cf58
Marko <49600278+clearfeld@users.noreply.github.com>
2023-09-11 20:08:32 -0400

    Add: name atom generators

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use self::data::defaults;
+
+pub mod data {
+    pub mod defaults {
+        pub mod name;
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +12,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // let result = add(2, 2);
+        // assert_eq!(result, 4);
     }
 }
