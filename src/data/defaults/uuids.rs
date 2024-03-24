c83623d9880f1bb9164b4022e898cdd13bc41b2c
Marko <49600278+clearfeld@users.noreply.github.com>
2024-03-23 20:55:03 -0400

    Add: docs for wasm bindgem uuids

--- src/data/defaults/uuids.rs
+++ src/data/defaults/uuids.rs
@@ -3,6 +3,7 @@ use rand_isaac::IsaacRng;
 use uuid::Uuid;
 use wasm_bindgen::prelude::*;
 
+/// as per [new_v1](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.new_v1)
 pub fn uuid_v1() -> Uuid {
 	Uuid::new_v1(
 		uuid::Timestamp::from_unix(
@@ -16,6 +17,7 @@ pub fn uuid_v1() -> Uuid {
 	)
 }
 
+/// as per [new_v3](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.new_v3)
 pub fn uuid_v3() -> Uuid {
 	Uuid::new_v3(
 		&Uuid::NAMESPACE_DNS,
@@ -23,10 +25,12 @@ pub fn uuid_v3() -> Uuid {
 	)
 }
 
+/// as per [new_v4](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.new_v4)
 pub fn uuid_v4() -> Uuid {
 	Uuid::new_v4()
 }
 
+/// as per [new_v5](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.new_v5)
 pub fn uuid_v5() -> Uuid {
 	Uuid::new_v5(
 		&Uuid::NAMESPACE_DNS,
@@ -34,16 +38,19 @@ pub fn uuid_v5() -> Uuid {
 	)
 }
 
+/// as per [now_v6](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.now_v6)
 pub fn uuid_v6() -> Uuid {
 	Uuid::now_v6(&[
 		1, 2, 3, 4, 5, 6,
 	])
 }
 
+/// as per [now_v7](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.now_v7)
 pub fn uuid_v7() -> Uuid {
 	Uuid::now_v7()
 }
 
+/// as per [new_v8](https://docs.rs/uuid/latest/uuid/struct.Uuid.html#method.new_v8)
 pub fn uuid_v8() -> Uuid {
 	Uuid::new_v8(IsaacRng::gen::<[u8; 16]>(&mut IsaacRng::seed_from_u64(6)))
 }
@@ -51,36 +58,99 @@ pub fn uuid_v8() -> Uuid {
 //
 
 #[wasm_bindgen(js_name = uuid_v1)]
+/// Returns a UUID v1 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v1();
+/// ```
 pub fn uuid_v1_wasm() -> String {
 	uuid_v1().to_string()
 }
 
 #[wasm_bindgen(js_name = uuid_v3)]
+/// Returns a UUID v3 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v3();
+/// ```
 pub fn uuid_v3_wasm() -> String {
 	uuid_v3().to_string()
 }
 
 #[wasm_bindgen(js_name = uuid_v4)]
+/// Returns a UUID v4 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v4();
+/// ```
 pub fn uuid_v4_wasm() -> String {
 	uuid_v4().to_string()
 }
 
 #[wasm_bindgen(js_name = uuid_v5)]
+/// Returns a UUID v5 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v5();
+/// ```
 pub fn uuid_v5_wasm() -> String {
 	uuid_v5().to_string()
 }
 
 #[wasm_bindgen(js_name = uuid_v6)]
+/// Returns a UUID v6 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v6();
+/// ```
 pub fn uuid_v6_wasm() -> String {
 	uuid_v6().to_string()
 }
 
 #[wasm_bindgen(js_name = uuid_v7)]
+/// Returns a UUID v7 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v7();
+/// ```
 pub fn uuid_v7_wasm() -> String {
 	uuid_v7().to_string()
 }
 
 #[wasm_bindgen(js_name = uuid_v8)]
+/// Returns a UUID v8 ([Universally Unique Identifier](https://en.wikipedia.org/wiki/Universally_unique_identifier)).
+///
+/// @return
+/// string
+///
+/// @example
+/// ```
+/// faker.uuid_v8();
+/// ```
 pub fn uuid_v8_wasm() -> String {
 	uuid_v8().to_string()
 }
