93f8b6eec5f87267a869269dd2069ba2b84c9454
Ryan McCallum <mccallry.dev@gmail.com>
2024-03-29 05:08:06 -0400

    Add basic unix gen with nanos

---
+++ src/data/datetime/unix.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+use crate::utils::seeder;
+
+#[wasm_bindgen]
+pub fn unix() -> String {
+  let (secs, nanos) = unix_gen();
+  concat_string!(secs.to_string(), nanos.to_string())
+}
+
+// Unix TS example:         1711702567
+// Unix TS example (nanos): 1711702550856745973
+pub fn unix_gen() -> (u64, u32) {
+  // Unix Timestamps only go to 2147483647 in most systems.
+  //  We can arbitrarily increase this, if required.
+  let secs: u64 = seeder::gen_range(0..=2_147_483_647);
+  let nanos: u32 = seeder::gen_range(0..1_000_000_000);
+  (secs, nanos)
+}
