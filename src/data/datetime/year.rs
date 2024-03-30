9cdca8b6c0112afa262d57d10ab0e42057819d62
Ryan McCallum <36088608+mccallry@users.noreply.github.com>
2024-03-30 18:59:45 -0400

    Update year ranges for epoch, let unconstrained be unsafe (#78)
    * Update year ranges for epoch, let unconstrained be unsafe
    
    * Fix unsafe year min

--- src/data/datetime/year.rs
+++ src/data/datetime/year.rs
@@ -3,5 +3,10 @@ use super::internals;
 
 #[wasm_bindgen]
 pub fn year() -> String {
-  internals::gen_year().to_string()
+  internals::gen_year_epoch().to_string()
+}
+
+#[wasm_bindgen]
+pub fn year_unsafe() -> String {
+  internals::gen_year_unsafe().to_string()
 }
